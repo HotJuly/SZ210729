94669a3e089d32d9c1114340f4a2af3612a31a93
HotJuly <949588380@qq.com>
2021-12-21 14:51:37 +0800

    07.小程序文件介绍

---
+++ 1.小程序/02.代码/wechat-init/app.js
@@ -0,0 +1,22 @@
+// app.js
+// 注册小程序
+//有且只能调用一次App
+// 会创建出全局唯一的小程序实例对象
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
