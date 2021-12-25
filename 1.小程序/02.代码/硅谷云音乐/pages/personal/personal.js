8cef57e969f48c27c0e6dfb8f2241d500113ee30
HotJuly <949588380@qq.com>
2021-12-25 17:08:04 +0800

    44.个人中心C3效果实现

---
+++ 1.小程序/02.代码/硅谷云音乐/pages/personal/personal.js
@@ -0,0 +1,100 @@
+// pages/personal/personal.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+        // 用于存储手指移动距离
+        moveDistance:0,
+
+        //  用于控制元素移动的过渡样式
+        moveTransition:""
+    },
+
+    // 用于监视用户手指按下事件
+    handleTouchStart(event){
+        // console.log('handleTouchStart',event)
+        this.startY = event.touches[0].clientY;
+        this.setData({
+            moveTransition:""
+        })
+    },
+
+    // 用于监视用户手指滑动事件
+    handleTouchMove(event){
+        // console.log('handleTouchMove',event)
+        const moveY = event.touches[0].clientY;
+        const moveDistance = moveY - this.startY;
+        console.log('moveDistance',moveDistance)
+        if(moveDistance>=80||moveDistance<=0)return;
+        this.setData({
+            moveDistance
+        })
+    },
+
+    // 用于监视用户手指滑动事件
+    handleTouchEnd(){
+        this.setData({
+            moveTransition:"transform 1s",
+            moveDistance:0
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
