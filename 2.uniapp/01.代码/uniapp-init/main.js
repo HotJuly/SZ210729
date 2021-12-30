8717a107c3b9f4fe167a47ccdf49dea921d92368
HotJuly <949588380@qq.com>
2021-12-30 11:07:02 +0800

    07.首页头部静态样式实现

--- 2.uniapp/01.代码/uniapp-init/main.js
+++ 2.uniapp/01.代码/uniapp-init/main.js
@@ -1,21 +1,30 @@
 import App from './App'
 
+// 如果当前运行环境不是Vue3,就执行以下代码
 // #ifndef VUE3
-import Vue from 'vue'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-    ...App
-})
-app.$mount()
 // #endif
 
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
+
+import Vue from 'vue'
+
+Vue.config.productionTip = false
+
+// mp->mini program type->类型
+// 由于小程序中精准区分APP,PAGE,COMPONENT三者,
+// 但是Vue中只有Vue构造函数,所以在此处声明当前App组件代表整个小程序
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+		// onLaunch: function() {
+		// 	console.log('App Launch')
+		// },
+		// onShow: function() {
+		// 	console.log('App Show')
+		// },
+		// onHide: function() {
+		// 	console.log('App Hide')
+		// }
+})
+app.$mount()
+
--- 2.uniapp/01.代码/uniapp-init/pages/index/index.vue
+++ 2.uniapp/01.代码/uniapp-init/pages/index/index.vue
@@ -1,52 +1,90 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<!-- <view class="indexContainer">
+		indexContainer
+	</view> -->
+	<!-- <div>
+		<div>123</div>
+		<i>123</i>
+		<i>234</i>
+	</div> -->
+	<!-- <swiper  indicator-dots="true"  autoplay="true"  interval="3000"  duration="1000">
+		<swiper-item>
+			<view class="swiper-item">123</view>
+		</swiper-item>
+		<swiper-item>
+			<view class="swiper-item">234</view>
+		</swiper-item>
+	</swiper> -->
+	<!-- uniapp同时兼容小程序和html的标签,但是建议使用小程序标签
+		因为html在小程序上兼容性一般,但是小程序标签在html上兼容良好-->
+		
+	<view class="indexContainer">
+		<view class="header">
+			<image class="logo" src="../../static/images/logo.png" mode=""></image>
+			<view class="search">
+				<view class="iconfont icon-sousuo">
+				</view>
+				<input class="searchInput" type="text" value="" />
+			</view>
+			<button class="username" type="default">七月</button>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			title:'Hello1'
 		}
-	}
+	},
+	onLoad() {},
+	methods:{}
+}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="stylus">
+	// shift+tab可以向前缩进,tab是向后缩进
+	.indexContainer
+		.header
+			/*
+				使用padding替代margin 
+					1.不能有边框
+					2.不能有背景颜色
+			*/
+			padding-top 20upx
+			display flex
+			align-items center
+			.logo
+				width 118upx
+				height 40upx
+				margin 0 20upx
+				flex-shrink  0
+			
+			.search
+				position relative
+				background-color  #ccc
+				flex-grow 1
+				border-radius  10upx
+				height 60upx
+				padding-left 60upx
+				.searchInput
+					height 100%
+				.iconfont
+					position absolute
+					left 20upx
+					top 50%
+					transform translateY(-50%)
+			
+			.username
+				width 140upx
+				height 60upx
+				font-size 24upx
+				line-height  60upx
+				color  red
+				margin 0 20upx
+				flex-shrink  0
+			
+		
+	
 </style>
